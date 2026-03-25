a82cf114f3e5b4df155ad5de0421fe13c3f0074a
surajAdhikari27 <adhikarisuraj857@gmail.com>
2026-03-26 00:33:10 +0530

    created HomePage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from '../Components/HomePageComponents/HeroSection/HeroSection'
+import FeatureSection from '../Components/HomePageComponents/FeatureSection/FeatureSection'
+
+
+function HomePage(){
+
+    return(
+        <>
+        <div>
+            <HeroSection />
+            <FeatureSection />
+        </div>
+        </>
+    )
+}
+
+export default HomePage;
