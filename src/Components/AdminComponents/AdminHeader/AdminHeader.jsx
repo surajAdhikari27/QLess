20a70376dd3ca99cf05cd828fcd3a8364c8a092d
surajAdhikari27 <adhikarisuraj857@gmail.com>
2026-03-30 16:18:17 +0530

    created admin header component with it's css

---
+++ src/Components/AdminComponents/AdminHeader/AdminHeader.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './AdminHeader.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faArrowRotateRight, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+function AdminHeader({handleReset, handleCallNext}){
+
+    return(
+        <>
+        <div className={styles.adminHeader}>
+            <div className={styles.adminPanelDetails}>
+                <h1>
+                    <FontAwesomeIcon icon={faUser}/>
+                    Admin Panel
+                </h1>
+                <p>Manage the queue and call the next token.</p>
+            </div>
+            <div className={styles.actionsButton}>
+                <button onClick={handleReset}>
+                    <FontAwesomeIcon icon={faArrowRotateRight}/>
+                    Reset
+                </button>
+
+                <button onClick={handleCallNext}>
+                    Call Next
+                    <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default AdminHeader;
