6b2c4df3e16948e020f776cc0560e8558c0aabb5
surajAdhikari27 <adhikarisuraj857@gmail.com>
2026-04-02 23:30:59 +0530

    created the login page with it's css

---
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import AppwriteAuthService from '../../AppwriteServices/AppwriteAuth';
+import {setLoading} from "../../store/AuthSlice"
+import {useDispatch, useSelector} from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import styles from './LoginPage.module.css';
+
+function Login(){
+    const navigate= useNavigate();
+    const dispatch= useDispatch();
+    const {loading}= useSelector((state)=>state.auth);
+    
+    const handleGoogleLogin=()=>{
+        try{
+            dispatch(setLoading(true));
+            AppwriteAuthService.loginWithGoogle();
+        }
+        catch(error){
+            console.log("Login error :: ",error);
+            dispatch(setLoading(false));
+        }
+    }
+    return(
+        <>
+        <div className={styles.loginPage}>
+            <div className={styles.card}>
+                <h1>Welcome Back</h1>
+                <p>Login to access Admin Panel</p>
+            </div>
+            <button
+                onClick={handleGoogleLogin}
+                disabled={loading}
+            >
+                {loading ? "Redirecting..." : "Continue with Google"}
+            </button>
+            <p onClick={() => navigate("/")}>
+                <FontAwesomeIcon icon={faArrowLeft}/>
+                Back to Home
+            </p>
+        </div>
+        </>
+    )
+}
+
+export default Login;
