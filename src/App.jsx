d57c86a33081b8f72be87439249957c5f88d6047
surajAdhikari27 <adhikarisuraj857@gmail.com>
2026-04-04 12:22:38 +0530

    created routes and updated TokenPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-import { useState } from 'react'
-
+import React,{ useState, useEffect  } from 'react'
+import {useDispatch} from 'react-redux';
+import {login,logout} from '../src/store/AuthSlice' 
+import AppwriteAuthService from "../src/AppwriteServices/AppwriteAuth"
+import AppwriteUserService from "../src/AppwriteServices/AppwriteUser"
 import './App.css'
-import Navbar from "./Components/HomePageComponents/Navbar"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from '../src/Pages/HomePage'
+import DashboardPage from '../src/Pages/DashboardPage/DashboardPage'
+import AdminPage from '../src/Pages/AdminPage/AdminPage'
+import LoginPage from '../src/Pages/LoginPage/LoginPage'
+import TokenPage from '../src/Pages/TokenPage/TokenPage'
+import ProtectedAdmin from '../src/Components/ProtectedAdmin';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch= useDispatch();
+
+  useEffect(()=>{
+    const checkUser=async()=>{
+      try{
+        const user= await AppwriteAuthService.getCurrentUser();
+        if(user){
+          const role= await AppwriteUserService.getUserRole(user.$id);
+          dispatch(login({...user,role}));
+        }
+        else{
+          dispatch(logout());
+        }
+      }
+      catch(error){
+        console.log("Login error :: ", error);
+        dispatch(logout());
+      }
+    };
+    checkUser();
+  },[]);
 
   return (
     <>
-      <Navbar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/dashboard" element={<DashboardPage/>}/>
+        <Route path="/token" element={<TokenPage/>}/>
+        <Route
+        path="/admin"
+        element={
+          <ProtectedAdmin>
+            <AdminPage />
+          </ProtectedAdmin>
+        }
+        />
+        <Route path="/login" element={<LoginPage />}/>
+      </Routes>
+    </BrowserRouter>
+     
     </>
   )
 }
--- src/Pages/TokenPage/TokenPage.jsx
+++ src/Pages/TokenPage/TokenPage.jsx
@@ -89,3 +89,5 @@ function TokenPage(){
         </>
     )
 }
+
+export default TokenPage;
